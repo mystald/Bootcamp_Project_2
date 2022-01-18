713b27d01e1495d91a628d036638adeff148d545
fania devira <faniardelia@gmail.com>
2022-01-18 10:10:07 +0700

    commit post ke service order

--- CustomerService/Models/Customer.cs
+++ CustomerService/Models/Customer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,6 +25,14 @@ namespace CustomerService.Models
         public double Balance { get; set; }
         
         [Required]
-        public int UserId {get; set;}
+        public int? UserId {get; set;}
+
+        [Required]
+        public double StartDest { get; set; }
+        
+        [Required]
+        public double EndDest { get; set; }
+
+        
     }
 }
