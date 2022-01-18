c6d8616e0fcc01269914301fe7030ec12d147b8e
Hilman Abdan <hilmanabdan28@gmail.com>
2022-01-18 15:53:11 +0700

    Revert "Revert "Service/driver""

---
+++ DriverService/Data/DriverDAL.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DriverService.Dtos;
+using DriverService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriverService.Data
+{
+    public class DriverDAL : IDriver
+    {
+        private readonly ApplicationDbContext _db;
+        public DriverDAL(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        //Get All Driver
+        public async Task<IEnumerable<Driver>> GetAll()
+        {
+            var results = await _db.Drivers.OrderBy(d => d.FirstName).ToListAsync();
+            return results;
+        }
+
+        //Get Balance Driver By Id
+        public async Task<Driver> GetBalanceById(string id)
+        {
+            var results = await(from d in _db.Drivers
+                                where d.Id == Convert.ToInt32(id)
+                                select d).SingleOrDefaultAsync();
+            if (results == null) throw new Exception($"Data {id} tidak temukan !");
+
+            return results;
+        }
+
+        //Get Profile Driver By Id
+        public async Task<Driver> GetProfileById(string id)
+        {
+            var results = await(from d in _db.Drivers
+                                where d.Id == Convert.ToInt32(id)
+                                select d).SingleOrDefaultAsync();
+            if (results == null) throw new Exception($"Data {id} tidak temukan !");
+
+            return results;
+        }
+
+        //Update Position Driver By Id
+        public void UpdatePosition(int id, Driver obj)
+        {
+            var result = _db.Drivers.FirstOrDefault(p => p.Id == id);
+            result.Latitude = obj.Latitude;
+            result.Longitude = obj.Longitude;
+            _db.SaveChanges();
+        }
+
+        //Update Accept Order -> Communication with Order Service
+        public void UpdateAcceptOrder(int id, Driver obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Update Finish Order -> Communication with Order Service
+        public void UpdateFinishOrder(int id, Driver obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Save Changes
+        public bool SaveChanges()
+        {
+            return (_db.SaveChanges() >= 0);
+        }
+
+    }
+}
---
+++ DriverService/Startup.cs
@@ -0,0 +1,139 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using DriverService.Data;
+using DriverService.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using DriverService.SyncDataService.Http;
+
+namespace DriverService
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+        }
+        public IConfiguration Configuration { get; } 
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Connection String
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("Local")));
+
+            //Interface and DAL
+            services.AddScoped<IDriver, DriverDAL>();
+
+            //HttpClient
+            services.AddHttpClient<IOrderDataClient, HttpOrderDataClient>();
+
+            //Newtonsoft JSON and XML
+            services.AddControllers().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddXmlSerializerFormatters();
+
+            //AutoMapper
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddControllers(); 
+
+            //Identity framework security
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = true;
+            }).AddDefaultTokenProviders().AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //JWT Bearer
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters =
+                new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            //Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DriverService", Version = "v1" });
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header menggunakan bearer token",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {securitySchema,new[]{"Bearer"} }
+                };
+                c.AddSecurityRequirement(securityRequirement);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DriverService v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
